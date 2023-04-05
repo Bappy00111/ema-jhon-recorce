988499e38145b0bfb363254e175e0ca1effdeb25
“[bappy <[bappymodok0011@gamil.com]>
2023-04-05 01:11:54 -0700

    header added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./Header.css"
+import logo from "../../images/Logo.svg"
 const Header = () => {
     return (
-        <div>
-           <h3>Wellcome to emajhon</h3>
+        <div className='header'>
+            <img src={logo} alt="" />
+            
+            <div>
+            <a href="/Order">Order</a>
+            <a href="/Order Review">Order Review</a>
+            <a href="/Manage Inventory">Manage Inventory</a>
+            <a href="/Login">Login</a>
+            </div>
+           
             
         </div>
     );
